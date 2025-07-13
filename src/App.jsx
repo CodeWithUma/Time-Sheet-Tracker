5a54701f0cca545a95cc654258cdea65122d4bc4
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-07-13 13:10:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='horizontal-heading'>
+        <h1>TIME SHEET TRACKER</h1>
+      </div>
+      <div className='vertical-heading'>
+        <ul>
+          <li>Name of the Employee</li>
+          <li>Change Password</li>
+          <li>Add New Task</li>
+          <li>View My Task</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
